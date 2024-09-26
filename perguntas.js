508cfdcae63cc6a07113a792ce832b17216c5125
camilliy <148974971+camilliy@users.noreply.github.com>
2024-09-26 13:33:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,50 @@
+const perguntas = [
+    {
+        pergunta: 'Quem encontra o mapa que leva ao tesouro do Royal Merchant?',
+        resposta: 'John B encontra o mapa no sótão da casa do pai dele.'
+    },
+    {
+        pergunta: 'O que acontece com o pai de John B no início da série?',
+        resposta: 'O pai de John B, Big John, desaparece enquanto procurava o tesouro do Royal Merchant, e todos acreditam que ele morreu.'
+    },
+    {
+        pergunta: 'Por que Ward Cameron decide incriminar John B?',
+        resposta: 'Ward incrimina John B pelo assassinato do Xerife Peterkin para proteger seu filho, Rafe, que realmente matou a xerife.'
+    },
+    {
+        pergunta: 'O que Kiara promete a seu pai quando os Pogues são pegos após invadir a mansão de Ward?',
+        resposta: 'Kiara promete a seu pai que nunca mais verá John B ou os outros Pogues novamente.'
+    },
+    {
+        pergunta: 'Como John B e Sarah escapam do furacão no final da 1ª temporada?',
+        resposta: 'Eles fogem em um barco e acabam sendo resgatados por um cargueiro que os leva para as Bahamas.'
+    },
+    {
+        pergunta: 'Qual é a grande revelação sobre Big John no final da 2ª temporada?',
+        resposta: 'É revelado que Big John está vivo, apesar de todos acreditarem que ele havia morrido.'
+    },
+    {
+        pergunta: 'Quem atira na xerife Peterkin, e por que isso acontece?',
+        resposta: 'Rafe Cameron atira na xerife Peterkin quando ela está prestes a prender Ward Cameron.'
+    },
+    {
+        pergunta: 'O que os Pogues encontram dentro da igreja abandonada nas Bahamas?',
+        resposta: 'Eles encontram uma cruz de ouro gigantesca, que é outro artefato valioso relacionado ao tesouro.'
+    },
+    {
+        pergunta: 'Como Sarah quase morre na 2ª temporada e quem a salva?',
+        resposta: 'Sarah quase morre após ser baleada por Rafe, e John B a salva, fazendo uma cirurgia improvisada para remover a bala.'
+    },
+    {
+        pergunta: 'Qual é o segredo que Pope descobre sobre sua família em relação ao tesouro?',
+        resposta: 'Pope descobre que sua família é descendente de Denmark Tanny, um escravo libertado que tinha conexões diretas com o tesouro do Royal Merchant.'
+    },
+    {
+        pergunta: 'Por que Pope se torna um alvo dos Limbrey?',
+        resposta: 'Pope se torna um alvo dos Limbrey porque eles descobrem que ele é descendente de Denmark Tanny, que supostamente sabia onde estava escondido o tesouro da Cruz de Santo Domingo.'
+    },
+    {
+        pergunta: 'Por que Sarah decide trair sua família e se unir aos Pogues?',
+        resposta: ' Sarah trai sua família quando descobre que seu pai, Ward Cameron, é responsável por vários crimes, incluindo o assassinato do pai de John B.'
+    }
+];
